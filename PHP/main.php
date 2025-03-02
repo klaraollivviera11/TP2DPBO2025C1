a0a0ee2aa9fa98473bbfaeabf477ffeff0fca550
Klara Ollivviera Augustine Gunawan <klaraollivviera@upi.edu>
2025-03-02 15:07:56 +0700

    Update main.php

--- PHP/main.php
+++ PHP/main.php
@@ -1,6 +1,8 @@
 <!-- Saya Klara Ollivviera Augustine Gunawan dengan NIM 2306205 
 mengerjakan soal Tugas Praktikum 1 dalam mata kuliah DPBO 
 untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin -->
+
+<!-- css tabel -->
 <!DOCTYPE html>
 <html lang="id">
 <head>
